9c77a411a43d5e234ee0ae40d29e2bb88a8dd537
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-11-15 10:47:28 +0000

    Login/Register rendered correctly

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
+import Jokes from './components/Jokes';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({component: Component, ...rest}) => {
+  return <Route {...rest} render={props => {
+    if (localStorage.getItem('token')) {
+      return <Component {...props} />
+    } else {
+      return <Redirect to="/login" />
+    }
+  }} />
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Let's Gooooo.
+      <Route path="/login" component={Login} />
+      <ProtectedRoute path="/jokes" component={Jokes} />
     </div>
   );
 }
-
 export default App;
