c14466eb9e7817d8142069dd087b27d76ef08e57
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-11-15 10:51:20 +0000

    Stretch done

--- api/server.js
+++ api/server.js
@@ -13,7 +13,8 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use('/api/jokes', jokesRouter);
+// authenticate,
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: "Running" })
